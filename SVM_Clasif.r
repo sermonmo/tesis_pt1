6801905b292ca7319edad2ce49b5c259484778ac
sermonmo <52253901+sermonmo@users.noreply.github.com>
2019-07-16 17:29:16 +0200

    Clasificación mediante Support Vector Machine
    CODIGO INCOMPLETO
    
    Fuente: https://www.youtube.com/watch?time_continue=169&v=eVCF6FfQocY

---
+++ SVM_Clasif.r
@@ -0,0 +1,28 @@
+
+install.packages
+
+library(raster)
+library(rgdal)
+library(snow)
+library(e1071)
+
+setwd("C:/CARPETA QUE CONTIENE LA IMAGEN")
+
+imgs2 <- brick("imagen_sentinel.tif")
+
+endme <- readOGR("C:/ruta_de_la_capa/shp","rois_entrenamiento")
+
+plorRGB(imgs2, 5, 4,3 stretch="lin")
+
+load("Datos.RData")
+
+modeloclasif <- svm(class~.,data=training, kernel="linear")
+
+beginCluster()
+svm_clas <- clusterR(im2, raster::predict, args = list(modeloclasif = modelclasif))
+endCluster()
+
+paleta_color <-  c("#00FF00", "0000FF", "228B22","FF1493")
+
+plot(svm_class, main="Clasificación con SCV", col = paleta_color,cex.lab=0.6,cex.axis=0.6,cex.main=0.7)
+
